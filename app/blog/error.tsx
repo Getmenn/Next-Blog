a5515470269105fb5eabd4e155472505cfb67da4
Artem <andrijauskas_artemy@cybersec74.ru>
2023-07-04 16:47:56 +0500

    first commit

---
+++ app/blog/error.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+export default function ErrorWrapper({
+    error
+}: {
+    error: Error
+}) {
+
+    return (
+        <h1>
+            Oops!!! {error.message}
+        </h1>
+    )
+}
