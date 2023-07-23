99fd850df66c85f19644cd61047829e54daf441c
Artem <andrijauskas_artemy@cybersec74.ru>
2023-07-23 18:11:14 +0500

    api points

---
+++ app/api/posts/posts.ts
@@ -0,0 +1,17 @@
+export const posts = [
+    {
+        id: 1,
+        title: "Napoli won Seria A",
+        body: "Some info about Italian football",
+    },
+    {
+        id: 2,
+        title: "Manchester City won Seria A",
+        body: "Some info about English football",
+    },
+    {
+        id: 3,
+        title: "Barselona won Seria A",
+        body: "Some info about Spanish football",
+    },
+]
